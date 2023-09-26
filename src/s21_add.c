ae3046c9b33c5484187da6b5e4cc30611db9af31
Grindelwald Felton <grindelf@student.21-school.ru>
2023-09-26 09:55:58 +0300

    .

--- src/s21_add.c
+++ src/s21_add.c
@@ -1,7 +1,23 @@
 #include "s21_decimal.h"
+#include <stdio.h>
 
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result)
+{
 
-    
+  s21_decimal decimal_value;
+
+  // Пример инициализации положительного числа 123.456 (значение для примера)
+  initialize_s21_decimal(&decimal_value, 0, 7, 123456);
+
+  // Доступ к частям числа
+  int sign = (decimal_value.bits[3] >> 31) & 1;
+  int exponent = (decimal_value.bits[3] >> 16) & 0xFF;
+  int mantissa = (decimal_value.bits[0]) | ((long long)decimal_value.bits[1] << 32) | ((long long)decimal_value.bits[2] << 64);
+
+  // Вывод значений
+  printf("Sign: %d\n", sign);
+  printf("Exponent: %d\n", exponent);
+  printf("Mantissa: %lld\n", mantissa);
+
   return 0;
 }
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -7,4 +7,13 @@ typedef struct {
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 
+// Функция для инициализации s21_decimal
+void initialize_s21_decimal(s21_decimal *dec, int sign, int exponent, int mantissa) {
+    dec->bits[0] = mantissa & 0xFFFFFFFF;
+    dec->bits[1] = (mantissa >> 32) & 0xFFFFFFFF;
+    dec->bits[2] = (mantissa >> 64) & 0xFFFFFFFF;
+    dec->bits[3] = (sign << 31) | ((exponent & 0xFF) << 16);
+}
+
+
 #endif
