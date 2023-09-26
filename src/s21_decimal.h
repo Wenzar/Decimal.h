ac97985b01c7fe98c3024b1e41f8780f7136252b
Grindelwald Felton <grindelf@student.21-school.ru>
2023-09-26 10:51:12 +0300

    start

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,19 +1,14 @@
 #ifndef STRING_H
 #define STRING_H
 
-typedef struct {
+#include <stdio.h>
+
+typedef struct
+{
   int bits[4];
 } s21_decimal;
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-
-// Функция для инициализации s21_decimal
-void initialize_s21_decimal(s21_decimal *dec, int sign, int exponent, int mantissa) {
-    dec->bits[0] = mantissa & 0xFFFFFFFF;
-    dec->bits[1] = (mantissa >> 32) & 0xFFFFFFFF;
-    dec->bits[2] = (mantissa >> 64) & 0xFFFFFFFF;
-    dec->bits[3] = (sign << 31) | ((exponent & 0xFF) << 16);
-}
 
 
 #endif
