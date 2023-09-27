2444c081a994089aff4430ed6e153324e3e9bac6
turus <elialiss@.students21-school.ru>
2023-09-27 18:50:40 +0300

    new tests

--- src/unit_tests/s21_decimal_test.h
+++ src/unit_tests/s21_decimal_test.h
@@ -11,6 +11,10 @@ Suite *s21_add_suite(void);
 Suite *s21_div_suite(void);
 Suite *s21_mul_suite(void);
 Suite *s21_sub_suite(void);
+Suite *s21_is_equal_suite(void);
+Suite *s21_floor_suite(void);
+Suite* s21_round_suite(void);
+Suite *s21_from_decimal_to_float_suite(void);
 
 void run_testcase(Suite *testcase);
 
