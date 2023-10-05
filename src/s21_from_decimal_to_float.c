bf18097a87ef535ccc3f087af5ff173bee0bd9dd
VadimTimofeev <timofeev.live@yandex.ru>
2023-10-05 22:24:03 +0300

    new_new

--- src/s21_from_decimal_to_float.c
+++ src/s21_from_decimal_to_float.c
@@ -1,64 +1,15 @@
 #include "s21_decimal.h"
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
-  s21_big_decimal our_copy = {0};
-  big_initialization(src, &our_copy);
-  // printf("\n%u %u %u %u\n\n", src.bits[3], src.bits[2], src.bits[1],
-  // src.bits[0]);
-
-  int error = 0;
-  int sign = 0;
-
-  if (big_getBit(our_copy, 223)) {
-    sign = 1;
-    big_setBit(&our_copy, 223, 0);
+  double buffer = (double)*dst;
+  for (int i = 0; i < 96; i++) {
+    buffer += getBit(src, i) * pow(2, i);
   }
-  // printf("\n%u\n\n", our_copy.bits[0]);
-
-  while ((our_copy.bits[0] / (int)pow(10, 8)) != 0 &&
-         find_out_the_degree(our_copy) != 0) {
-    demotion_scale(&our_copy);
+  buffer *= pow(10, -small_find_out_the_degree(src));
+  if (getBit(src, 127)) {
+    buffer *= -1;
   }
-
-  if (our_copy.bits[2] || our_copy.bits[1] || our_copy.bits[3] ||
-      our_copy.bits[4] || our_copy.bits[5]) {
-    error = 1;
-    while (our_copy.bits[0] / pow(10, 8) != 0) {
-      demotion_scale(&our_copy);
-    }
-  }
-
-  if (our_copy.bits[0] % 10 >= 5 && find_out_the_degree(our_copy) > 7) {
-    demotion_scale(&our_copy);
-    big_addition(our_copy, one_big_decimal, &our_copy);
-  }
-
-  else if (find_out_the_degree(our_copy) > 7) {
-    demotion_scale(&our_copy);
-  }
-
-  if (find_out_the_degree(our_copy) == 0) {
-    *dst = our_copy.bits[0];
-  } else {
-    int count = find_out_the_degree(our_copy);
-    int my_res = our_copy.bits[0];
-
-    while (count) {
-      my_res /= 10;
-      count--;
-    }
-
-    *dst += my_res;
-    // printf("\n%u\n",our_copy.bits[0]%10000);
-    double tmp_res =
-        our_copy.bits[0] % (int)pow(10, find_out_the_degree(our_copy));
-    tmp_res /= pow(10, find_out_the_degree(our_copy));
-    // printf("\ndst :%f\n",*dst);
-    *dst += tmp_res;
-    // printf("\ndst+tmp :%f\n",*dst);
-  }
-  if (sign) {
-    *dst = -*dst;
-  }
-  return error;
+  *dst = buffer;
+  return 0;
 }
+
--- src/test.c
+++ src/test.c
@@ -14,11 +14,11 @@
 #include "s21_floor.c"
 #include "s21_round.c"
 int main() {
-     s21_decimal src1 = {0};
-  src1.bits[0] = 0b10101111110010001101100101011111;
-  src1.bits[1] = 0b01101000101000101011010010000001;
-  src1.bits[2] = 0b00000000000000000000000000000001;
-  src1.bits[3] = 0b00000000000100110000000000000000;
+     s21_decimal src1 = {{376228, 0, 0, 3<<16}};
+//   src1.bits[0] = 0b10101111110010001101100101011111;
+//   src1.bits[1] = 0b01101000101000101011010010000001;
+//   src1.bits[2] = 0b00000000000000000000000000000001;
+//   src1.bits[3] = 0b00000000000100110000000000000000;
 //     s21_decimal src2 = {0};
 //   src2.bits[0] = 0b00000000000000000000000000000011;
 //   src2.bits[1] = 0b00000000000000000000000000000000;
@@ -34,20 +34,23 @@ int main() {
 //   dec1.bits[1] = 0b00000000000000000000000000000000;
 //   dec1.bits[2] = 0b00000000000000000000000000000000;
 //   dec1.bits[3] = 0b10000000000000010000000000000000;
-   s21_decimal result={0};
+//    s21_decimal result={0};
 //      result.bits[0] = 0b00000000000000000000000000010001;
 //   result.bits[1] = 0b00000000000000000000000000000000;
 //   result.bits[2] = 0b00000000000000000000000000000000;
 //   result.bits[3] = 0b10000000000000000000000000000000;
-   s21_decimal res = {{0}};
-   s21_decimal origin = {{0}};
-    origin.bits[0] = 0b00000000000000000000000000000010;
-  origin.bits[1] = 0b00000000000000000000000000000000;
-  origin.bits[2] = 0b00000000000000000000000000000000;
-  origin.bits[3] = 0b00000000000000000000000000000000;
+//    s21_decimal res = {{0}};
+//    s21_decimal origin = {{0}};
+//     origin.bits[0] = 0b00000000000000000000000000000010;
+//   origin.bits[1] = 0b00000000000000000000000000000000;
+//   origin.bits[2] = 0b00000000000000000000000000000000;
+//   origin.bits[3] = 0b00000000000000000000000000000000;
   int error=0;
-  error= s21_round(src1,&result);
+   float result = 0;
+  float check = 0xFFFFFFFFFFFFFFFF;
+  error= s21_from_decimal_to_float(src1,&result);
   printf("\nERROR=%d\n", error);
+ 
 
   printf("\nDEC1 sign:%u scale:%u %u %u %u\n", src1.bits[3] >> 31,
          small_find_out_the_degree(src1), src1.bits[2], src1.bits[1],
@@ -61,12 +64,12 @@ int main() {
 //   printf("\nRESULT %u %u %u %u %u\n", res.bits[3] >> 31,
 //          small_find_out_the_degree(res), res.bits[2], res.bits[1], res.bits[0]);
        //   printf("\n s21=%u origin_res=%u\n",res_s21,res1 );
-         printf("\nORIGIN %u %u %u %u %u\n", origin.bits[3] >> 31,
-         small_find_out_the_degree(origin), origin.bits[2], origin.bits[1], origin.bits[0]);
+       //   printf("\nORIGIN %u %u %u %u %u\n", origin.bits[3] >> 31,
+       //   small_find_out_the_degree(origin), origin.bits[2], origin.bits[1], origin.bits[0]);
        // printf("\n=== %lf", floor(0.99));
-       printf("\nRESULT %u %u %u %u %u\n", result.bits[3] >> 31,
-          small_find_out_the_degree(result), result.bits[2], result.bits[1], result.bits[0]);
-          printf("\n%f", round(3.5) );
+       // printf("\nRESULT %u %u %u %u %u\n", result.bits[3] >> 31,
+       //    small_find_out_the_degree(result), result.bits[2], result.bits[1], result.bits[0]);
+          printf("\nRESULT=%f, CHECK= %f", result, check);
 
   return 0;
 }
