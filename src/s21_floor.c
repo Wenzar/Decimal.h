1a2bd55a7bd8662a98ec9361f1cf37d2a66c0f5f
Grindelwald Felton <grindelf@student.21-school.ru>
2023-10-08 21:23:26 +0300

    fix floor

--- src/s21_floor.c
+++ src/s21_floor.c
@@ -1,7 +1,12 @@
 #include "s21_decimal.h"
 
 int s21_floor(s21_decimal value, s21_decimal *result) {
+  result->bits[0] = 0;
+  result->bits[1] = 0;
+  result->bits[2] = 0;
+  result->bits[3] = 0;
   int error = 0;
+
   s21_big_decimal big_copy_decimal = {0};
   big_initialization(value, &big_copy_decimal);
 
@@ -14,5 +19,14 @@ int s21_floor(s21_decimal value, s21_decimal *result) {
     big_shift_right(&big_copy_decimal, 1);
   }
   big_uninitilization(big_copy_decimal, result);
+
+  // Проверяем была ли дробная часть у числа, если была и число отрицательное то
+  // добавляем 1 к bits[0]
+  s21_decimal income_value = {{value.bits[3], 0, 0, 0}};
+  s21_decimal compare_value = {{0b10000000000000010000000000000000, 0, 0, 0}};
+
+  if (s21_is_greater_or_equal(income_value, compare_value)) {
+    result->bits[0]++;
+  }
   return error;
 }
--- src/unit_tests/s21_floor_test.c
+++ src/unit_tests/s21_floor_test.c
@@ -67,7 +67,7 @@ START_TEST(s21_floor_4) {
   dec1.bits[2] = 0b00000000000000000000000000000000;
   dec1.bits[3] = 0b10000000000000110000000000000000;
   s21_decimal result;
-  result.bits[0] = 0b00000000001001011110001101010011;
+  result.bits[0] = 0b00000000001001011110001101010100;
   result.bits[1] = 0b00000000000000000000000000000000;
   result.bits[2] = 0b00000000000000000000000000000000;
   result.bits[3] = 0b10000000000000000000000000000000;
@@ -124,15 +124,14 @@ END_TEST
 
 START_TEST(s21_floor_7) {
   s21_decimal dec1;
-
-  dec1.bits[0] = 0b10111101001001111000010001010010;
+  dec1.bits[0] = 0b10111101001001111000010001010010; //-123.432567890
   dec1.bits[1] = 0b00000000000000000000000000011100;
   dec1.bits[2] = 0b00000000000000000000000000000000;
   dec1.bits[3] = 0b10000000000010010000000000000000;
   s21_decimal res1;
   s21_floor(dec1, &res1);
   s21_decimal result;
-  result.bits[0] = 0b00000000000000000000000001111011;
+  result.bits[0] = 0b00000000000000000000000001111100;
   result.bits[1] = 0b00000000000000000000000000000000;
   result.bits[2] = 0b00000000000000000000000000000000;
   result.bits[3] = 0b10000000000000000000000000000000;
@@ -166,7 +165,6 @@ END_TEST
 
 START_TEST(s21_floor_9) {
   s21_decimal dec1;
-
   dec1.bits[0] = 0b01010101001110101110101110110001;
   dec1.bits[1] = 0b00001101101101001101101001011111;
   dec1.bits[2] = 0b00000000000000000000000000000000;
@@ -174,7 +172,7 @@ START_TEST(s21_floor_9) {
   s21_decimal res1;
   s21_floor(dec1, &res1);
   s21_decimal result;
-  result.bits[0] = 0b00000000000000000000000000000000;
+  result.bits[0] = 0b00000000000000000000000000000001;
   result.bits[1] = 0b00000000000000000000000000000000;
   result.bits[2] = 0b00000000000000000000000000000000;
   result.bits[3] = 0b10000000000000000000000000000000;
@@ -322,7 +320,7 @@ START_TEST(s21_floorTest3) {
   src1.bits[1] = 0b00000000000000000000000000000000;
   src1.bits[2] = 0b00000000000000000000000000000000;
   src1.bits[3] = 0b10000000000000010000000000000000;
-  origin.bits[0] = 0b00000000000000000000000000000011;
+  origin.bits[0] = 0b00000000000000000000000000000100;
   origin.bits[1] = 0b00000000000000000000000000000000;
   origin.bits[2] = 0b00000000000000000000000000000000;
   origin.bits[3] = 0b10000000000000000000000000000000;
@@ -364,7 +362,7 @@ START_TEST(s21_floorTest5) {
   src1.bits[1] = 0b00000000000000000000000000000000;
   src1.bits[2] = 0b00000000000000000000000000000000;
   src1.bits[3] = 0b10000000000000010000000000000000;
-  origin.bits[0] = 0b00000000000000000000000000000001;
+  origin.bits[0] = 0b00000000000000000000000000000010;
   origin.bits[1] = 0b00000000000000000000000000000000;
   origin.bits[2] = 0b00000000000000000000000000000000;
   origin.bits[3] = 0b10000000000000000000000000000000;
@@ -385,7 +383,7 @@ START_TEST(s21_floorTest6) {
   src1.bits[1] = 0b11111001111010000010010110101101;
   src1.bits[2] = 0b10110000001111101111000010010100;
   src1.bits[3] = 0b10000000000011100000000000000000;
-  origin.bits[0] = 0b10000110011101001011101110010001;
+  origin.bits[0] = 0b10000110011101001011101110010010;
   origin.bits[1] = 0b00000000000000011111000000010110;
   origin.bits[2] = 0b00000000000000000000000000000000;
   origin.bits[3] = 0b10000000000000000000000000000000;
@@ -406,7 +404,7 @@ START_TEST(s21_floorTest7) {
   src1.bits[1] = 0b01100110010010001001000110100011;
   src1.bits[2] = 0b00000000000001001000101100011101;
   src1.bits[3] = 0b10000000000110000000000000000000;
-  origin.bits[0] = 0b00000000000000000000000000000101;
+  origin.bits[0] = 0b00000000000000000000000000000110;
   origin.bits[1] = 0b00000000000000000000000000000000;
   origin.bits[2] = 0b00000000000000000000000000000000;
   origin.bits[3] = 0b10000000000000000000000000000000;
@@ -449,7 +447,7 @@ START_TEST(s21_floorTest9) {
   src1.bits[2] = 0b00100111111001000001101011010101;
   src1.bits[3] = 0b10000000000101010000000000000000;
 
-  origin.bits[0] = 0b00000000101111000110000101001101;
+  origin.bits[0] = 0b00000000101111000110000101001110;
   origin.bits[1] = 0b00000000000000000000000000000000;
   origin.bits[2] = 0b00000000000000000000000000000000;
   origin.bits[3] = 0b10000000000000000000000000000000;
@@ -490,11 +488,11 @@ START_TEST(s21_floorTest11) {
   src1.bits[0] = 0b00000000000000000000000000000001;
   src1.bits[1] = 0b00000000000000000000000000000000;
   src1.bits[2] = 0b00000000000000000000000000000000;
-  src1.bits[3] = 0b00000000000000000000000000000000;
+  src1.bits[3] = 0b10000000000000000000000000000000;
   origin.bits[0] = 0b00000000000000000000000000000001;
   origin.bits[1] = 0b00000000000000000000000000000000;
   origin.bits[2] = 0b00000000000000000000000000000000;
-  origin.bits[3] = 0b00000000000000000000000000000000;
+  origin.bits[3] = 0b10000000000000000000000000000000;
   s21_floor(src1, &result);
   ck_assert_int_eq(origin.bits[3], result.bits[3]);
   ck_assert_int_eq(origin.bits[2], result.bits[2]);
@@ -503,11 +501,112 @@ START_TEST(s21_floorTest11) {
 }
 END_TEST
 
+void floor_test_function(float n, float accuracy) {
+  s21_decimal var1 = {0}, var2 = {0};
+  float value1 = 0, value2 = 0;
+  s21_from_float_to_decimal(n, &var1);
+  s21_floor(var1, &var2);
+  s21_from_decimal_to_float(var2, &value2);
+  value1 = floorf(n);
+  ck_assert_float_eq_tol(value1, value2, accuracy);
+}
+
+START_TEST(floor_0) {
+  s21_decimal val = {{2, 0, 0, 0}};
+  s21_decimal res;
+  ck_assert_int_eq(0, s21_floor(val, &res));
+}
+END_TEST
+
+START_TEST(floor_1) {
+  s21_decimal val = {{2, 0, 0, 2147483648}};
+  s21_decimal res = {{0}};
+  s21_floor(val, &res);
+  float fres = 0;
+  s21_from_decimal_to_float(res, &fres);
+  ck_assert_float_eq(-2, fres);
+}
+END_TEST
+
+START_TEST(floor_2) {
+  s21_decimal val = {{0b00000000000011110111001010101111,  // -10123.99
+                      0b00000000000000000000000000000000,
+                      0b00000000000000000000000000000000, 
+                      0b10000000000000100000000000000000}};
+  s21_decimal res = {{0}};
+  s21_floor(val, &res);
+  float fres = 0;
+  s21_from_decimal_to_float(res, &fres);
+  ck_assert_float_eq(-10124, fres);
+}
+END_TEST
+
+START_TEST(floor_3) {
+  s21_decimal val = {{2, 0, 0, 0}};
+  s21_decimal res = {{0}};
+  set_degree(&val, 5);
+  s21_floor(val, &res);
+  float fres = 0;
+  s21_from_decimal_to_float(res, &fres);
+  ck_assert_float_eq(0, fres);
+}
+END_TEST
+
+START_TEST(floor_4) {
+  float n = s21_rand_r(-8388608, 8388608);
+  floor_test_function(n, 10);
+}
+
+START_TEST(floor_5) {
+  s21_decimal value_1 = {{7444923, 0, 0, 0}};
+  set_degree(&value_1, 5);
+  s21_decimal check = {{74, 0, 0, 0}};
+  s21_decimal result = {0};
+  int return_value = s21_floor(value_1, &result);
+  ck_assert_int_eq(s21_is_equal(result, check), 1);
+  ck_assert_int_eq(return_value, 0);
+}
+END_TEST
+
+START_TEST(floor_6) {
+  s21_decimal value_1 = {{7444923, 0, 0, 0}};
+  set_degree(&value_1, 5);
+  set_minos(&value_1);
+  s21_decimal check = {{75, 0, 0, 0}};
+  set_minos(&check);
+  s21_decimal result = {0};
+  int return_value = s21_floor(value_1, &result);
+  ck_assert_int_eq(s21_is_equal(result, check), 1);
+  ck_assert_int_eq(return_value, 0);
+}
+END_TEST
+
+START_TEST(floor_7) {
+  s21_decimal src1;
+  src1.bits[0] = 0b00001111111111111111111111111111;
+  src1.bits[1] = 0b00111110001001010000001001100001;
+  src1.bits[2] = 0b00100000010011111100111001011110;
+  src1.bits[3] = 0b10000000000010100000000000000000;
+  s21_decimal res_od = {0};
+  s21_floor(src1, &res_od);
+  s21_decimal result = {0};
+  result.bits[0] = 0b10100111011001000000000000000000;
+  result.bits[1] = 0b00001101111000001011011010110011;
+  result.bits[2] = 0b00000000000000000000000000000000;
+  result.bits[3] = 0b10000000000000000000000000000000;
+  ck_assert_float_eq(res_od.bits[0], result.bits[0]);
+  ck_assert_float_eq(res_od.bits[1], result.bits[1]);
+  ck_assert_float_eq(res_od.bits[2], result.bits[2]);
+  ck_assert_float_eq(res_od.bits[3], result.bits[3]);
+}
+END_TEST
+
 Suite *s21_floor_suite(void) {
   Suite *s;
   TCase *tc;
   s = suite_create("s21_floor_suite");
   tc = tcase_create("s21_floor_case");
+
   tcase_add_test(tc, s21_floor_1);
   tcase_add_test(tc, s21_floor_2);
   tcase_add_test(tc, s21_floor_3);
@@ -533,6 +632,15 @@ Suite *s21_floor_suite(void) {
   tcase_add_test(tc, s21_floorTest9);
   tcase_add_test(tc, s21_floorTest10);
   tcase_add_test(tc, s21_floorTest11);
+  tcase_add_test(tc, floor_0);
+  tcase_add_test(tc, floor_1);
+  tcase_add_test(tc, floor_2);
+  tcase_add_test(tc, floor_3);
+  tcase_add_loop_test(tc, floor_4, 0, 1000);
+  tcase_add_test(tc, floor_5);
+  tcase_add_test(tc, floor_6);
+  tcase_add_test(tc, floor_7);
+
   suite_add_tcase(s, tc);
   return s;
 }
